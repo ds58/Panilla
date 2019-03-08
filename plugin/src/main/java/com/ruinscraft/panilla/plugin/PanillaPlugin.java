c64062725e9c03782c872d1092e2b5fbe662a9aa
Dexton Anderson <contact@dextonanderson.com>
2019-03-08 13:50:42 -0600

    init

---
+++ plugin/src/main/java/com/ruinscraft/panilla/plugin/PanillaPlugin.java
@@ -0,0 +1,17 @@
+package com.ruinscraft.panilla.plugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class PanillaPlugin extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+		
+	}
+	
+	@Override
+	public void onDisable() {
+		
+	}
+	
+}
