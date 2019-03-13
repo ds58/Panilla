b5e05c4be849539e397ef01cb84d304221dccc82
Dexton Anderson <contact@dextonanderson.com>
2019-03-13 10:48:16 -0500

    load locale from config

--- api/src/main/java/com/ruinscraft/panilla/api/config/PConfig.java
+++ api/src/main/java/com/ruinscraft/panilla/api/config/PConfig.java
@@ -6,6 +6,7 @@ import java.util.List;
 public class PConfig {
 
 	// defaults
+	public String localeFile = "en_US.yml";
 	public boolean consoleLogging = true;
 	public boolean chatLogging = false;
 	public PStrictness strictness = PStrictness.AVERAGE;
