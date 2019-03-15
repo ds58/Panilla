dcee3aec31a8395da2200eade899d2cceaf8662d
Dexton Anderson <contact@dextonanderson.com>
2019-03-15 13:25:58 -0500

    flush out plugin.yml

--- plugin/src/main/java/com/ruinscraft/panilla/plugin/PanillaCommand.java
+++ plugin/src/main/java/com/ruinscraft/panilla/plugin/PanillaCommand.java
@@ -1,5 +1,8 @@
 package com.ruinscraft.panilla.plugin;
 
+import java.util.List;
+
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -9,8 +12,9 @@ public class PanillaCommand implements CommandExecutor {
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 		final String version = PanillaPlugin.get().getDescription().getVersion();
+		final List<String> authors = PanillaPlugin.get().getDescription().getAuthors();
 
-		sender.sendMessage("Running Panilla version: " + version);
+		sender.sendMessage(ChatColor.GOLD + "Running Panilla version: " + version + " by: " + String.join(", ", authors));
 
 		return true;
 	}
