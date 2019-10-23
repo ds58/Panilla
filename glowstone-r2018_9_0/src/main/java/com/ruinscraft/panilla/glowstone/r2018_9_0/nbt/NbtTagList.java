c9da6d31451828ff07d1b54bcd5ce54eae4a6a1c
Dexton Anderson <contact@dextonanderson.com>
2019-10-23 12:15:09 -0500

    fix glowstone nbt implementation

--- glowstone-r2018_9_0/src/main/java/com/ruinscraft/panilla/glowstone/r2018_9_0/nbt/NbtTagList.java
+++ glowstone-r2018_9_0/src/main/java/com/ruinscraft/panilla/glowstone/r2018_9_0/nbt/NbtTagList.java
@@ -2,30 +2,30 @@ package com.ruinscraft.panilla.glowstone.r2018_9_0.nbt;
 
 import com.ruinscraft.panilla.api.nbt.INbtTagCompound;
 import com.ruinscraft.panilla.api.nbt.INbtTagList;
+import net.glowstone.util.nbt.ListTag;
+import net.glowstone.util.nbt.Tag;
 
-import java.util.List;
+public class NbtTagList<T extends Tag> implements INbtTagList {
 
-public class NbtTagList<V> implements INbtTagList {
+    private ListTag<T> handle;
 
-    private final List<V> handle;
-
-    public NbtTagList(List<V> handle) {
+    public NbtTagList(ListTag<T> handle) {
         this.handle = handle;
     }
 
     @Override
     public INbtTagCompound getCompound(int index) {
-        return null;
+        return (INbtTagCompound) handle.getValue().get(index);  // TODO: safe cast?
     }
 
     @Override
     public String getString(int index) {
-        return handle.get(index).toString();
+        return handle.getValue().get(index).toString();
     }
 
     @Override
     public int size() {
-        return handle.size();
+        return handle.getValue().size();
     }
 
 }
