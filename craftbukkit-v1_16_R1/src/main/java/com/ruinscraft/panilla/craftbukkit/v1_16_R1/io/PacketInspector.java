fbee982178d3aa1e5673ebd6b4b2461bea48655d
Dexton Anderson <contact@dextonanderson.com>
2020-06-24 21:26:44 -0500

    start 1.16.1 support

---
+++ craftbukkit-v1_16_R1/src/main/java/com/ruinscraft/panilla/craftbukkit/v1_16_R1/io/PacketInspector.java
@@ -0,0 +1,163 @@
+package com.ruinscraft.panilla.craftbukkit.v1_16_R1.io;
+
+import com.ruinscraft.panilla.api.IPanilla;
+import com.ruinscraft.panilla.api.IPanillaPlayer;
+import com.ruinscraft.panilla.api.exception.EntityNbtNotPermittedException;
+import com.ruinscraft.panilla.api.exception.FailedNbt;
+import com.ruinscraft.panilla.api.exception.NbtNotPermittedException;
+import com.ruinscraft.panilla.api.io.IPacketInspector;
+import com.ruinscraft.panilla.api.nbt.INbtTagCompound;
+import com.ruinscraft.panilla.api.nbt.checks.NbtChecks;
+import com.ruinscraft.panilla.craftbukkit.v1_16_R1.nbt.NbtTagCompound;
+import net.minecraft.server.v1_16_R1.*;
+import org.bukkit.Bukkit;
+import org.bukkit.craftbukkit.v1_16_R1.entity.CraftEntity;
+import org.bukkit.craftbukkit.v1_16_R1.entity.CraftPlayer;
+
+import java.lang.reflect.Field;
+import java.util.UUID;
+
+public class PacketInspector implements IPacketInspector {
+
+    private final IPanilla panilla;
+
+    public PacketInspector(IPanilla panilla) {
+        this.panilla = panilla;
+    }
+
+    @Override
+    public void checkPacketPlayInSetCreativeSlot(Object _packet) throws NbtNotPermittedException {
+        if (_packet instanceof PacketPlayInSetCreativeSlot) {
+            PacketPlayInSetCreativeSlot packet = (PacketPlayInSetCreativeSlot) _packet;
+
+            int slot = packet.b();
+            ItemStack itemStack = packet.getItemStack();
+
+            if (itemStack == null || !itemStack.hasTag()) return;
+
+            NbtTagCompound tag = new NbtTagCompound(itemStack.getTag());
+            String itemClass = itemStack.getItem().getClass().getSimpleName();
+            String packetClass = packet.getClass().getSimpleName();
+
+            NbtChecks.checkPacketPlayIn(slot, tag, itemClass, packetClass, panilla);
+        }
+    }
+
+    @Override
+    public void checkPacketPlayOutSetSlot(Object _packet) throws NbtNotPermittedException {
+        if (_packet instanceof PacketPlayOutSetSlot) {
+            PacketPlayOutSetSlot packet = (PacketPlayOutSetSlot) _packet;
+
+            try {
+                Field slotField = PacketPlayOutSetSlot.class.getDeclaredField("b");
+                Field itemStackField = PacketPlayOutSetSlot.class.getDeclaredField("c");
+
+                slotField.setAccessible(true);
+                itemStackField.setAccessible(true);
+
+                int slot = (int) slotField.get(packet);
+                ItemStack itemStack = (ItemStack) itemStackField.get(packet);
+
+                if (itemStack == null || !itemStack.hasTag()) return;
+
+                NbtTagCompound tag = new NbtTagCompound(itemStack.getTag());
+                String itemClass = itemStack.getClass().getSimpleName();
+                String packetClass = packet.getClass().getSimpleName();
+
+                NbtChecks.checkPacketPlayOut(slot, tag, itemClass, packetClass, panilla);
+            } catch (NoSuchFieldException | SecurityException | IllegalArgumentException | IllegalAccessException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public void checkPacketPlayOutSpawnEntity(Object _packet) throws EntityNbtNotPermittedException {
+        if (_packet instanceof PacketPlayOutSpawnEntity) {
+            PacketPlayOutSpawnEntity packet = (PacketPlayOutSpawnEntity) _packet;
+
+            try {
+                Field typeField = PacketPlayOutSpawnEntity.class.getDeclaredField("b");
+
+                typeField.setAccessible(true);
+
+                UUID entityId = (UUID) typeField.get(packet);
+                org.bukkit.entity.Entity bukkitEntity = org.bukkit.Bukkit.getEntity(entityId);
+                CraftEntity craftEntity = (CraftEntity) bukkitEntity;
+
+                if (craftEntity == null) {
+                    return;
+                }
+
+                Entity entity = craftEntity.getHandle();
+
+                if (entity != null) {
+                    if (entity instanceof EntityItem) {
+                        EntityItem item = (EntityItem) entity;
+
+                        if (item.getItemStack() == null) {
+                            return;
+                        }
+
+                        if (!item.getItemStack().hasTag()) {
+                            return;
+                        }
+
+                        INbtTagCompound tag = new NbtTagCompound(item.getItemStack().getTag());
+                        String itemName = item.getItemStack().getItem().getName();
+                        FailedNbt failedNbt = NbtChecks.checkAll(tag, itemName, panilla);
+
+                        if (FailedNbt.fails(failedNbt)) {
+                            throw new EntityNbtNotPermittedException(packet.getClass().getSimpleName(), false, failedNbt, entityId);
+                        }
+                    }
+                }
+            } catch (NoSuchFieldException | SecurityException | IllegalArgumentException | IllegalAccessException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public void sendPacketPlayOutSetSlotAir(IPanillaPlayer player, int slot) {
+        CraftPlayer craftPlayer = (CraftPlayer) player.getHandle();
+        EntityPlayer entityPlayer = craftPlayer.getHandle();
+        PacketPlayOutSetSlot packet = new PacketPlayOutSetSlot(0, slot, new ItemStack(Blocks.AIR));
+        entityPlayer.playerConnection.sendPacket(packet);
+    }
+
+    @Override
+    public void stripNbtFromItemEntity(UUID entityId) {
+        org.bukkit.entity.Entity bukkitEntity = Bukkit.getServer().getEntity(entityId);
+
+        if (bukkitEntity instanceof CraftEntity) {
+            CraftEntity craftEntity = (CraftEntity) bukkitEntity;
+            Entity entity = craftEntity.getHandle();
+
+            if (entity instanceof EntityItem) {
+                EntityItem item = (EntityItem) entity;
+
+                if (item.getItemStack() == null) {
+                    return;
+                }
+
+                if (!item.getItemStack().hasTag()) {
+                    return;
+                }
+
+                item.getItemStack().setTag(null);
+            }
+        }
+    }
+
+    @Override
+    public void stripNbtFromItemEntityLegacy(int entityId) {
+        throw new RuntimeException("cannot use #stripNbtFromItemEntityLegacy on 1.16");
+    }
+
+    @Override
+    public void validateBaseComponentParse(String string) throws Exception {
+        IChatBaseComponent.ChatSerializer.a(string);
+    }
+
+}
