e959820e9036f8824ecd972fdd078e2520580266
Dexton Anderson <contact@dextonanderson.com>
2020-08-17 20:11:51 -0500

    1.16.2 support

---
+++ craftbukkit-v1_16_R2/src/main/java/com/ruinscraft/panilla/craftbukkit/v1_16_R2/io/PlayerInjector.java
@@ -0,0 +1,30 @@
+package com.ruinscraft.panilla.craftbukkit.v1_16_R2.io;
+
+import com.ruinscraft.panilla.api.IPanillaPlayer;
+import com.ruinscraft.panilla.api.io.IPlayerInjector;
+import io.netty.channel.Channel;
+import io.netty.handler.codec.ByteToMessageDecoder;
+import net.minecraft.server.v1_16_R2.EntityPlayer;
+import net.minecraft.server.v1_16_R2.PacketDecompressor;
+import org.bukkit.craftbukkit.v1_16_R2.entity.CraftPlayer;
+
+public class PlayerInjector implements IPlayerInjector {
+
+    @Override
+    public Channel getPlayerChannel(IPanillaPlayer player) throws IllegalArgumentException {
+        CraftPlayer craftPlayer = (CraftPlayer) player.getHandle();
+        EntityPlayer entityPlayer = craftPlayer.getHandle();
+        return entityPlayer.playerConnection.networkManager.channel;
+    }
+
+    @Override
+    public int getCompressionLevel() {
+        return 256;
+    }
+
+    @Override
+    public ByteToMessageDecoder getDecompressor() {
+        return new PacketDecompressor(getCompressionLevel());
+    }
+
+}
---
+++ craftbukkit-v1_16_R2/src/main/java/com/ruinscraft/panilla/craftbukkit/v1_16_R2/nbt/NbtTagList.java
@@ -0,0 +1,30 @@
+package com.ruinscraft.panilla.craftbukkit.v1_16_R2.nbt;
+
+import com.ruinscraft.panilla.api.nbt.INbtTagCompound;
+import com.ruinscraft.panilla.api.nbt.INbtTagList;
+import net.minecraft.server.v1_16_R2.NBTTagList;
+
+public class NbtTagList implements INbtTagList {
+
+    private final NBTTagList handle;
+
+    public NbtTagList(NBTTagList handle) {
+        this.handle = handle;
+    }
+
+    @Override
+    public INbtTagCompound getCompound(int index) {
+        return new NbtTagCompound(handle.getCompound(index));
+    }
+
+    @Override
+    public String getString(int index) {
+        return handle.getString(index);
+    }
+
+    @Override
+    public int size() {
+        return handle.size();
+    }
+
+}
