49147fa64e376b60cdbde6ec44c7adaa37236505
ds58 <30220598+ds58@users.noreply.github.com>
2022-12-12 17:08:42 -0600

    Fix check for super long lore lines

--- api/src/main/java/com/ruinscraft/panilla/api/nbt/checks/NbtCheck_display.java
+++ api/src/main/java/com/ruinscraft/panilla/api/nbt/checks/NbtCheck_display.java
@@ -32,57 +32,61 @@ public class NbtCheck_display extends NbtCheck {
     public NbtCheckResult check(INbtTagCompound tag, String itemName, IPanilla panilla) {
         INbtTagCompound display = tag.getCompound(getName());
 
-        String name = display.getString("Name");
+        if (display.hasKeyOfType("Name", NbtDataType.STRING)) {
+            String name = display.getString("Name");
 
-        // check for Json array
-        if (name.startsWith("[{")) {
-            try {
-                JsonElement jsonElement = PARSER.parse(name);
-                JsonArray jsonArray = jsonElement.getAsJsonArray();
+            // check for Json array
+            if (name.startsWith("[{")) {
+                try {
+                    JsonElement jsonElement = PARSER.parse(name);
+                    JsonArray jsonArray = jsonElement.getAsJsonArray();
 
-                name = createTextFromJsonArray(jsonArray);
-            } catch (Exception e) {
-                // could not parse Json
+                    name = createTextFromJsonArray(jsonArray);
+                } catch (Exception e) {
+                    return NbtCheckResult.CRITICAL; // could not parse Json
+                }
             }
-        }
 
-        // check for Json object
-        else if (name.startsWith("{")) {
-            try {
-                JsonElement jsonElement = PARSER.parse(name);
-                JsonObject jsonObject = jsonElement.getAsJsonObject();
-                JsonArray jsonArray = jsonObject.getAsJsonArray("extra");
+            // check for Json object
+            else if (name.startsWith("{")) {
+                try {
+                    JsonElement jsonElement = PARSER.parse(name);
+                    JsonObject jsonObject = jsonElement.getAsJsonObject();
+                    JsonArray jsonArray = jsonObject.getAsJsonArray("extra");
 
-                name = createTextFromJsonArray(jsonArray);
-            } catch (Exception e) {
-                // could not parse Json
+                    name = createTextFromJsonArray(jsonArray);
+                } catch (Exception e) {
+                    return NbtCheckResult.CRITICAL; // could not parse Json
+                }
             }
-        }
 
-        final int maxNameLength;
+            final int maxNameLength;
 
-        // if strict, use anvil length
-        if (panilla.getPConfig().strictness.ordinal() >= PStrictness.STRICT.ordinal()) {
-            maxNameLength = panilla.getProtocolConstants().maxAnvilRenameChars();
-        } else {
-            maxNameLength = panilla.getProtocolConstants().NOT_PROTOCOL_maxItemNameLength();
-        }
+            // if strict, use anvil length
+            if (panilla.getPConfig().strictness.ordinal() >= PStrictness.STRICT.ordinal()) {
+                maxNameLength = panilla.getProtocolConstants().maxAnvilRenameChars();
+            } else {
+                maxNameLength = panilla.getProtocolConstants().NOT_PROTOCOL_maxItemNameLength();
+            }
 
-        if (name != null && name.length() > maxNameLength) {
-            return NbtCheckResult.CRITICAL; // can cause crashes
-        }
-
-        INbtTagList lore = display.getList("Lore", NbtDataType.LIST);
-
-        if (lore.size() > panilla.getProtocolConstants().NOT_PROTOCOL_maxLoreLines()) {
-            return NbtCheckResult.CRITICAL; // can cause crashes
-        }
-
-        for (int i = 0; i < lore.size(); i++) {
-            String line = lore.getString(i);
-
-            if (line.length() > panilla.getProtocolConstants().NOT_PROTOCOL_maxLoreLineLength()) {
+            if (name.length() > maxNameLength) {
                 return NbtCheckResult.CRITICAL; // can cause crashes
+            }
+        }
+
+        if (display.hasKeyOfType("Lore", NbtDataType.LIST)) {
+            INbtTagList lore = display.getList("Lore");
+
+            if (lore.size() > panilla.getProtocolConstants().NOT_PROTOCOL_maxLoreLines()) {
+                return NbtCheckResult.CRITICAL; // can cause crashes
+            }
+
+            for (int i = 0; i < lore.size(); i++) {
+                String line = lore.getString(i);
+
+                if (line.length() > panilla.getProtocolConstants().NOT_PROTOCOL_maxLoreLineLength()) {
+                    return NbtCheckResult.CRITICAL; // can cause crashes
+                }
             }
         }
 
