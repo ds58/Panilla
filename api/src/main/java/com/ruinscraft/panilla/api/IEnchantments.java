78a04020128278ba631852af871770c3ec81295b
ds58 <30220598+ds58@users.noreply.github.com>
2023-05-29 14:23:49 -0500

    implement IEnchantments on bukkit

--- api/src/main/java/com/ruinscraft/panilla/api/IEnchantments.java
+++ api/src/main/java/com/ruinscraft/panilla/api/IEnchantments.java
@@ -2,10 +2,6 @@ package com.ruinscraft.panilla.api;
 
 public interface IEnchantments {
 
-    EnchantmentCompat getById(int id);
-
-    EnchantmentCompat getByKey(String key);
-
     int getMaxLevel(EnchantmentCompat enchCompat);
 
     int getStartLevel(EnchantmentCompat enchCompat);
