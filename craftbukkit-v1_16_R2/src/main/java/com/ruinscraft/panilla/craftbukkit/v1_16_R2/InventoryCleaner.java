2a1106560886946b162e5ff2a6742177b25592dc
ds58 <30220598+ds58@users.noreply.github.com>
2023-05-29 14:23:51 -0500

    1.16.2 support

---
+++ craftbukkit-v1_16_R2/src/main/java/com/ruinscraft/panilla/craftbukkit/v1_16_R2/InventoryCleaner.java
@@ -0,0 +1,49 @@
+package com.ruinscraft.panilla.craftbukkit.v1_16_R2;
+
+import com.ruinscraft.panilla.api.IInventoryCleaner;
+import com.ruinscraft.panilla.api.IPanilla;
+import com.ruinscraft.panilla.api.IPanillaPlayer;
+import com.ruinscraft.panilla.api.exception.FailedNbt;
+import com.ruinscraft.panilla.api.nbt.INbtTagCompound;
+import com.ruinscraft.panilla.api.nbt.checks.NbtChecks;
+import com.ruinscraft.panilla.craftbukkit.v1_16_R2.nbt.NbtTagCompound;
+import net.minecraft.server.v1_16_R2.Container;
+import net.minecraft.server.v1_16_R2.ItemStack;
+import net.minecraft.server.v1_16_R2.NBTTagCompound;
+import org.bukkit.craftbukkit.v1_16_R2.entity.CraftPlayer;
+
+public class InventoryCleaner implements IInventoryCleaner {
+
+    private final IPanilla panilla;
+
+    public InventoryCleaner(IPanilla panilla) {
+        this.panilla = panilla;
+    }
+
+    @Override
+    public void clean(IPanillaPlayer player) {
+        CraftPlayer craftPlayer = (CraftPlayer) player.getHandle();
+        Container container = craftPlayer.getHandle().activeContainer;
+
+        for (int slot = 0; slot < container.slots.size(); slot++) {
+            ItemStack itemStack = container.getSlot(slot).getItem();
+
+            if (itemStack == null || !itemStack.hasTag()) {
+                continue;
+            }
+
+            NBTTagCompound nmsTag = itemStack.getTag();
+            INbtTagCompound tag = new NbtTagCompound(nmsTag);
+            String itemName = itemStack.getItem().getName();
+            FailedNbt failedNbt = NbtChecks.checkAll(tag, itemName, panilla);
+
+            if (FailedNbt.failsThreshold(failedNbt)) {
+                container.getSlot(slot).getItem().setTag(null);
+            } else if (FailedNbt.fails(failedNbt)) {
+                nmsTag.remove(failedNbt.key);
+                container.getSlot(slot).getItem().setTag(nmsTag);
+            }
+        }
+    }
+
+}
