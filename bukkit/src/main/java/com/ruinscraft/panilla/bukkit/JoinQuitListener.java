a87e838b67925f733c754700a1aaf22bd12307ad
ds58 <30220598+ds58@users.noreply.github.com>
2023-05-29 14:23:49 -0500

    package and module rename

---
+++ bukkit/src/main/java/com/ruinscraft/panilla/bukkit/JoinQuitListener.java
@@ -0,0 +1,25 @@
+package com.ruinscraft.panilla.bukkit;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+
+public class JoinQuitListener implements Listener {
+
+    @EventHandler
+    public void onJoin(PlayerJoinEvent event) {
+        Player player = event.getPlayer();
+
+        PanillaPlugin.get().getPlayerInjector().register(new BukkitPanillaPlayer(player));
+    }
+
+    @EventHandler
+    public void onQuit(PlayerQuitEvent event) {
+        Player player = event.getPlayer();
+
+        PanillaPlugin.get().getPlayerInjector().unregister(new BukkitPanillaPlayer(player));
+    }
+
+}
---
+++ bukkit/src/main/java/com/ruinscraft/panilla/bukkit/PanillaCommand.java
@@ -0,0 +1,22 @@
+package com.ruinscraft.panilla.bukkit;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+import java.util.List;
+
+public class PanillaCommand implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        final String version = PanillaPlugin.get().getDescription().getVersion();
+        final List<String> authors = PanillaPlugin.get().getDescription().getAuthors();
+
+        sender.sendMessage(ChatColor.GOLD + "Running Panilla version: " + version + " by " + String.join(", ", authors));
+
+        return true;
+    }
+
+}
