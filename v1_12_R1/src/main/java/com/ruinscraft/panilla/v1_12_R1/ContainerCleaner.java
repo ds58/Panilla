8e86da832185b53888aca740f136baf54658cc31
ds58 <30220598+ds58@users.noreply.github.com>
2023-05-29 14:23:49 -0500

    fix method mappings for 1.12

--- v1_12_R1/src/main/java/com/ruinscraft/panilla/v1_12_R1/ContainerCleaner.java
+++ v1_12_R1/src/main/java/com/ruinscraft/panilla/v1_12_R1/ContainerCleaner.java
@@ -8,13 +8,28 @@ import com.ruinscraft.panilla.v1_12_R1.nbt.NbtTagCompound;
 import net.minecraft.server.v1_12_R1.Container;
 import net.minecraft.server.v1_12_R1.ItemStack;
 import net.minecraft.server.v1_12_R1.NBTTagCompound;
+import net.minecraft.server.v1_12_R1.Slot;
 import org.bukkit.craftbukkit.v1_12_R1.entity.CraftPlayer;
 import org.bukkit.entity.Player;
+
+import java.lang.reflect.Field;
+import java.util.List;
 
 public class ContainerCleaner implements IContainerCleaner {
 
     private final PConfig config;
     private final IProtocolConstants protocolConstants;
+
+    private static boolean is_1_12_2;
+
+    static {
+        try {
+            Container.class.getField("slots");
+            is_1_12_2 = true;
+        } catch (NoSuchFieldException e) {
+            is_1_12_2 = false;
+        }
+    }
 
     public ContainerCleaner(PConfig config, IProtocolConstants protocolConstants) {
         this.config = config;
@@ -26,7 +41,23 @@ public class ContainerCleaner implements IContainerCleaner {
         CraftPlayer craftPlayer = (CraftPlayer) player;
         Container container = craftPlayer.getHandle().activeContainer;
 
-        for (int slot = 0; slot < container.slots.size(); slot++) {
+        int containerSlotsSize = 0;
+
+        if (is_1_12_2) {
+            containerSlotsSize = container.slots.size();
+        } else {
+            try {
+                Field slotsField = Container.class.getField("c");
+                List<Slot> slots = (List<Slot>) slotsField.get(container);
+                containerSlotsSize = slots.size();
+            } catch (NoSuchFieldException e) {
+                e.printStackTrace();
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            }
+        }
+
+        for (int slot = 0; slot < containerSlotsSize; slot++) {
             ItemStack itemStack = container.getSlot(slot).getItem();
 
             if (itemStack == null || !itemStack.hasTag()) continue;
