d50b9cd54f8323418661950a00e7b430cc8c8161
ds58 <30220598+ds58@users.noreply.github.com>
2023-05-29 14:23:51 -0500

    Fix check for loot table namespace, add additional Bukkit dispense check for valid loot table

--- bukkit/src/main/java/com/ruinscraft/panilla/bukkit/TileLootTableListener.java
+++ bukkit/src/main/java/com/ruinscraft/panilla/bukkit/TileLootTableListener.java
@@ -3,10 +3,14 @@ package com.ruinscraft.panilla.bukkit;
 import org.bukkit.Bukkit;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockState;
+import org.bukkit.block.ShulkerBox;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockDispenseEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.BlockStateMeta;
 import org.bukkit.loot.Lootable;
 
 public class TileLootTableListener implements Listener {
@@ -34,6 +38,28 @@ public class TileLootTableListener implements Listener {
     public void onInteract(PlayerInteractEvent event) {
         if (checkForLootable) {
             fixLootTable(event.getClickedBlock());
+        }
+    }
+
+    @EventHandler
+    public void onDispense(BlockDispenseEvent event) {
+        ItemStack itemStack = event.getItem();
+
+        if (itemStack.getItemMeta() instanceof BlockStateMeta) {
+            BlockStateMeta blockStateMeta = (BlockStateMeta) itemStack.getItemMeta();
+
+            if (blockStateMeta.getBlockState() instanceof ShulkerBox) {
+                ShulkerBox shulker = (ShulkerBox) blockStateMeta.getBlockState();
+
+                try {
+                    shulker.getLootTable().getKey();
+                } catch (Exception e) {
+                    shulker.setLootTable(null);
+                    blockStateMeta.setBlockState(shulker);
+                    itemStack.setItemMeta(blockStateMeta);
+                    event.setCancelled(true);
+                }
+            }
         }
     }
 
