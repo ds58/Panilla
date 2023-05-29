2a93e4ea9fb3fa3c64feac874152ba6f9d0a3326
ds58 <30220598+ds58@users.noreply.github.com>
2023-05-29 14:23:51 -0500

    Craftbukkit 1.18

---
+++ craftbukkit-v1_18_R1/src/main/java/com/ruinscraft/panilla/craftbukkit/v1_18_R1/io/PlayerInjector.java
@@ -0,0 +1,55 @@
+package com.ruinscraft.panilla.craftbukkit.v1_18_R1.io;
+
+import com.ruinscraft.panilla.api.IPanillaPlayer;
+import com.ruinscraft.panilla.api.io.IPlayerInjector;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.ByteToMessageDecoder;
+import net.minecraft.network.PacketDecoder;
+import net.minecraft.network.protocol.EnumProtocolDirection;
+import net.minecraft.server.level.EntityPlayer;
+import org.bukkit.craftbukkit.v1_18_R1.entity.CraftPlayer;
+
+import java.util.List;
+
+public class PlayerInjector implements IPlayerInjector {
+
+    @Override
+    public Channel getPlayerChannel(IPanillaPlayer player) throws IllegalArgumentException {
+        CraftPlayer craftPlayer = (CraftPlayer) player.getHandle();
+        EntityPlayer entityPlayer = craftPlayer.getHandle();
+        return entityPlayer.b.a.k;
+    }
+
+    @Override
+    public int getCompressionLevel() {
+        return 256;
+    }
+
+    @Override
+    public ByteToMessageDecoder getDecompressor() {
+        return null;
+    }
+
+    @Override
+    public ByteToMessageDecoder getDecoder() {
+        return new PanillaPacketDecoder(EnumProtocolDirection.a);
+    }
+
+    private class PanillaPacketDecoder extends PacketDecoder {
+        public PanillaPacketDecoder(EnumProtocolDirection enumProtocolDirection) {
+            super(enumProtocolDirection);
+        }
+
+        @Override
+        protected void decode(ChannelHandlerContext channelHandlerContext, ByteBuf byteBuf, List<Object> list) {
+            try {
+                super.decode(channelHandlerContext, byteBuf, list);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
