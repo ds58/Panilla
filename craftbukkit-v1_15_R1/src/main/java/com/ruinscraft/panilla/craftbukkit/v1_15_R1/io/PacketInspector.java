964628ecc8fe947ce619386ae6c9fd32411f730e
ds58 <30220598+ds58@users.noreply.github.com>
2023-05-29 14:23:50 -0500

    craftbukkit 1.15 support

---
+++ craftbukkit-v1_15_R1/src/main/java/com/ruinscraft/panilla/craftbukkit/v1_15_R1/io/PacketInspector.java
@@ -0,0 +1,138 @@
+package com.ruinscraft.panilla.craftbukkit.v1_15_R1.io;
+
+import com.ruinscraft.panilla.api.IPanilla;
+import com.ruinscraft.panilla.api.IPanillaPlayer;
+import com.ruinscraft.panilla.api.exception.EntityNbtNotPermittedException;
+import com.ruinscraft.panilla.api.exception.FailedNbt;
+import com.ruinscraft.panilla.api.exception.NbtNotPermittedException;
+import com.ruinscraft.panilla.api.io.IPacketInspector;
+import com.ruinscraft.panilla.api.nbt.INbtTagCompound;
+import com.ruinscraft.panilla.api.nbt.checks.NbtChecks;
+import com.ruinscraft.panilla.craftbukkit.v1_15_R1.nbt.NbtTagCompound;
+import net.minecraft.server.v1_15_R1.*;
+import org.bukkit.Bukkit;
+import org.bukkit.craftbukkit.v1_15_R1.entity.CraftEntity;
+import org.bukkit.craftbukkit.v1_15_R1.entity.CraftPlayer;
+
+import java.lang.reflect.Field;
+import java.util.UUID;
+
+public class PacketInspector implements IPacketInspector {
+
+    private final IPanilla panilla;
+
+    public PacketInspector(IPanilla panilla) {
+        this.panilla = panilla;
+    }
+
+    @Override
+    public void checkPacketPlayInSetCreativeSlot(Object _packet) throws NbtNotPermittedException {
+        if (_packet instanceof PacketPlayInSetCreativeSlot) {
+            PacketPlayInSetCreativeSlot packet = (PacketPlayInSetCreativeSlot) _packet;
+
+            int slot = packet.b();
+            ItemStack itemStack = packet.getItemStack();
+
+            if (itemStack == null || !itemStack.hasTag()) return;
+
+            NbtTagCompound tag = new NbtTagCompound(itemStack.getTag());
+            String itemClass = itemStack.getItem().getClass().getSimpleName();
+            String packetClass = packet.getClass().getSimpleName();
+
+            NbtChecks.checkPacketPlayIn(slot, tag, itemClass, packetClass, panilla);
+        }
+    }
+
+    @Override
+    public void checkPacketPlayOutSetSlot(Object _packet) throws NbtNotPermittedException {
+        if (_packet instanceof PacketPlayOutSetSlot) {
+            PacketPlayOutSetSlot packet = (PacketPlayOutSetSlot) _packet;
+
+            try {
+                Field slotField = PacketPlayOutSetSlot.class.getDeclaredField("b");
+                Field itemStackField = PacketPlayOutSetSlot.class.getDeclaredField("c");
+
+                slotField.setAccessible(true);
+                itemStackField.setAccessible(true);
+
+                int slot = (int) slotField.get(packet);
+                ItemStack itemStack = (ItemStack) itemStackField.get(packet);
+
+                if (itemStack == null || !itemStack.hasTag()) return;
+
+                NbtTagCompound tag = new NbtTagCompound(itemStack.getTag());
+                String itemClass = itemStack.getClass().getSimpleName();
+                String packetClass = packet.getClass().getSimpleName();
+
+                NbtChecks.checkPacketPlayOut(slot, tag, itemClass, packetClass, panilla);
+            } catch (NoSuchFieldException | SecurityException | IllegalArgumentException | IllegalAccessException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public void checkPacketPlayOutSpawnEntity(Object _packet) throws EntityNbtNotPermittedException {
+        if (_packet instanceof PacketPlayOutSpawnEntity) {
+            PacketPlayOutSpawnEntity packet = (PacketPlayOutSpawnEntity) _packet;
+
+            try {
+                Field typeField = PacketPlayOutSpawnEntity.class.getDeclaredField("b");
+
+                typeField.setAccessible(true);
+
+                UUID entityId = (UUID) typeField.get(packet);
+                org.bukkit.entity.Entity bukkitEntity = org.bukkit.Bukkit.getEntity(entityId);
+                CraftEntity craftEntity = (CraftEntity) bukkitEntity;
+
+                if (craftEntity == null) {
+                    return;
+                }
+
+                Entity entity = craftEntity.getHandle();
+
+                if (entity != null) {
+                    if (entity instanceof EntityItem) {
+                        EntityItem item = (EntityItem) entity;
+
+                        if (item.getItemStack() == null) {
+                            return;
+                        }
+
+                        if (!item.getItemStack().hasTag()) {
+                            return;
+                        }
+
+                        INbtTagCompound tag = new NbtTagCompound(item.getItemStack().getTag());
+                        FailedNbt failedNbt = NbtChecks.checkAll(tag, item.getItemStack().getClass().getSimpleName(), panilla);
+
+                        if (FailedNbt.fails(failedNbt)) {
+                            throw new EntityNbtNotPermittedException(packet.getClass().getSimpleName(), false, failedNbt, entityId);
+                        }
+                    }
+                }
+            } catch (NoSuchFieldException | SecurityException | IllegalArgumentException | IllegalAccessException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public void sendPacketPlayOutSetSlotAir(IPanillaPlayer player, int slot) {
+        CraftPlayer craftPlayer = (CraftPlayer) player.getHandle();
+        EntityPlayer entityPlayer = craftPlayer.getHandle();
+        PacketPlayOutSetSlot packet = new PacketPlayOutSetSlot(0, slot, new ItemStack(Blocks.AIR));
+        entityPlayer.playerConnection.sendPacket(packet);
+    }
+
+    @Override
+    public void removeEntity(UUID entityId) {
+        Bukkit.getServer().getEntity(entityId).remove();
+    }
+
+    @Override
+    public void removeEntityLegacy(int entityId) {
+        throw new IllegalStateException("Cannot use #removeEntityLegacy on 1.15");
+    }
+
+}
---
+++ craftbukkit-v1_15_R1/src/main/java/com/ruinscraft/panilla/craftbukkit/v1_15_R1/io/PlayerInjector.java
@@ -0,0 +1,18 @@
+package com.ruinscraft.panilla.craftbukkit.v1_15_R1.io;
+
+import com.ruinscraft.panilla.api.IPanillaPlayer;
+import com.ruinscraft.panilla.api.io.IPlayerInjector;
+import io.netty.channel.Channel;
+import net.minecraft.server.v1_15_R1.EntityPlayer;
+import org.bukkit.craftbukkit.v1_15_R1.entity.CraftPlayer;
+
+public class PlayerInjector implements IPlayerInjector {
+
+    @Override
+    public Channel getPlayerChannel(IPanillaPlayer player) throws IllegalArgumentException {
+        CraftPlayer craftPlayer = (CraftPlayer) player.getHandle();
+        EntityPlayer entityPlayer = craftPlayer.getHandle();
+        return entityPlayer.playerConnection.networkManager.channel;
+    }
+
+}
